820630d521f2b9121dbd5e3fc4eea0b9da794473
commanderscotty <scott.glaittli@gmail.com>
2020-09-25 20:16:25 -0600

    did a smol amount'

--- tkTest/main.py
+++ tkTest/main.py
@@ -1,19 +1,20 @@
 import tkinter as tk
 
+
 class Renderer:
     def __init__(self):
         self.window = tk.Tk()
         self.window.attributes('-fullscreen', True)
-        self.resolution = (1920, 1080)
-        self.canvas = tk.Canvas(self.window, bg='black', highlightthickness=0, width=self.resolution[0], height=self.resolution[1])
+        self.resolution = {'x': self.window.winfo_screenwidth(), 'y': self.window.winfo_screenheight()}
+        self.canvas = tk.Canvas(self.window, bg='black', highlightthickness=0, width=self.resolution['x'], height=self.resolution['y'])
         self.render('placeholder')
         self.window.mainloop()
 
 
     def render(self, game):
         self.canvas.delete("all")
-        line0 = self.canvas.create_line(0, 0, 1920, 1080, fill='red')
-        line1 = self.canvas.create_line(1920, 0, 0, 1080, fill='green', width=10)
+        line0 = self.canvas.create_line(0, 0, self.resolution['x'], self.resolution['y'], fill='red')
+        line1 = self.canvas.create_line(self.resolution['x'], 0, 0, self.resolution['y'], fill='green', width=10)
         self.canvas.pack()
         
 
